06939d0478663afee45eb7b501bd60a9eb63ae09
Guennr <Guennr@gmail.com>
2023-02-27 15:02:19 -0800

    pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,25 +1,12 @@
 #include "main.h"
-
 /**
- * reset_to_98 - a function to update the reference to a pointer
- * @n: pointer to n to be updated
- */
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Owned By Bwave/Bright
+*  Return: 0 is success
+*/
+
 void reset_to_98(int *n)
 {
-	*n = 98;
-}
-
-/**
- * main - the entry point of the program
- *
- * Return: Always 0
- */
-int main(void)
-{
-	int n = 10;
-
-	printf("Before reset_to_98: n = %d\n", n);
-	reset_to_98(&n);
-	printf("After reset_to_98: n = %d\n", n);
-	return (0);
+*n = 98;
 }
